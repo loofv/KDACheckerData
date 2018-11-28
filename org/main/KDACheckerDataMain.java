546a9d62d5a57a0c144116045a3921b84ed51d8e
justicebieber <philipkd@hotmail.com>
2018-11-28 16:32:07 +0100

    removed systemouts

--- org/main/KDACheckerDataMain.java
+++ org/main/KDACheckerDataMain.java
@@ -2,9 +2,6 @@ public class KDACheckerDataMain {
 
   public static void main (String[] args) {
 
-    System.out.println ("Hello world!");
-    System.out.println ("bajs");
-    System.out.println ("Hej Philip!: kl. 15:29");
-    System.out.println ("Hej Phili32r32r4fwefsdp!: kl. 15:29");
+    
   }
 }
