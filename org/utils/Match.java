7de8fe18f18cff3feb345b9778fd68189db91dbd
philipkd@hotmail.com <philipkd@hotmail.com>
2018-11-28 19:08:19 +0100

    stuff

--- org/utils/Match.java
+++ org/utils/Match.java
@@ -7,23 +7,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.util.Scanner;
-import java.util.URL;
-import javax.json.*;
+import java.net.URL;
+//import javax.json.*;
 //fixa imports, importerar allt for now.
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.net.*;
 
 public class Match {
-  private static final String END POINT = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/";
+  private static final String END_POINT = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/";
   private static final String QUERY     = "34158466?api_key="; //Insert API Key from KeyReader
 
   public String MatchList() {
 
-
+    return null;
   }
 
 
-  int gameID;
-  ArrayList <Player> players;
+  //int gameID;
+  //ArrayList <Player> players;
 }
