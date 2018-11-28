0200cf220eb1e864237060390bca922c24a5f9ec
philipkd@hotmail.com <philipkd@hotmail.com>
2018-11-28 18:45:39 +0100

    Added som stuff to Match

--- org/utils/Match.java
+++ org/utils/Match.java
@@ -1,19 +1,26 @@
 package utils;
 
 import Player;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.util.URL;
-import java.util.Scanner;
-import java.io.IOException;
-import java.util.URL;
-//fixa imports, importerar allt for now.
-import javax.json.*;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.Scanner;
+import java.util.URL;
+import javax.json.*;
+//fixa imports, importerar allt for now.
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.net.*;
 
 public class Match {
 
+  public static String MatchList() {
 
+        private static final String END POINT = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/";
+        private static final String QUERY     = "34158466?api_key="; //Insert API Key from KeyReader
+  }
 
 
   int gameID;
