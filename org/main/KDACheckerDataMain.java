fb7c2df58ee43b1e0a64d2b45151f3464cc2fc52
loofv <love.hulterstrom@gmail.com>
2018-11-28 21:47:15 +0100

    Kommenterat ut odeklerade/ofärdiga variabler och metoder samt döpt om package i match.

--- org/main/KDACheckerDataMain.java
+++ org/main/KDACheckerDataMain.java
@@ -3,7 +3,7 @@ import org.utils.*;
 public class KDACheckerDataMain {
 
   public static void main (String[] args) {
-    Match m = new Match().MatchLists();
-
+    //Match m = new Match().MatchLists();
+    //avmarkerat då den inte vet vad en match är för något
   }
 }
