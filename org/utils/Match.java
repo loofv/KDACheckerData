7c528b5b006ecab3eb606c9f4d146090edffab21
justicebieber <philipkd@hotmail.com>
2018-11-28 18:08:13 +0100

    .

--- org/utils/Match.java
+++ org/utils/Match.java
@@ -1,8 +1,21 @@
 package utils;
 
 import Player;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.URL;
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.URL;
+
+import javax.json.*;
+
 
 public class Match {
+
+
+
+
   int gameID;
   ArrayList <Player> players;
 }
