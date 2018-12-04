112b4ff028145ebb666e01610c96d2e1b63fbe74
justicebieber <philipkd@hotmail.com>
2018-12-04 12:29:13 +0100

    Lagt till javax.json api, lagt till ny funktion i Match

--- org/utils/Match.java
+++ org/utils/Match.java
@@ -8,11 +8,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.util.Scanner;
 import java.net.URL;
-//import javax.json.*;
+import javax.json.*;
+import javax.servlet.*;
 //fixa imports, importerar allt for now.
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.net.*;
+//import static javax.json.JsonValue.ValueType.*;
 
 public class Match {
 
@@ -20,6 +22,29 @@ public class Match {
   private final String        API_KEY   = kr.readKey(); //readKeyReader2();
   private static final String END_POINT = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/";
   private final String        QUERY     = ("34158466?api_key="+ API_KEY); //Insert API Key from KeyReader
+
+
+
+  public void JsonParser () throws FileNotFoundException { //Denna ska kunna läsa en JSon fil
+    try{
+        JsonReader reader     = new Json.createReader( new FileReader(jsonFile));
+        JsonStruct jsonStruct = reader.read();
+
+        if (jsonStruct = reader.getValueType().equals(OBJECT)) {
+
+          System.out.println("Casting to JsonObject...");
+          JsonObject jo = (Json) jsonStruct;
+        }
+        System.out.println("Summonername: " + jo.getString("summonerName"));
+
+    } catch(JsonException e) {
+        System.err.println("File not found: " + e.getMessage());
+    }
+  //    JsonArray() jarr = jo.getJsonArray("");
+
+  }
+
+
 
   public  void  MatchList() {
 
