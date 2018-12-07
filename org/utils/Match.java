cafdfce675265b62f6557a60a07098b00b94e9ca
justicebieber <philipkd@hotmail.com>
2018-12-07 12:56:57 +0100

    .

--- org/utils/Match.java
+++ org/utils/Match.java
@@ -1,13 +1,11 @@
 package org.utils;
 
-//import Player;
-//import org.utils.KeyReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.io.FileReader;
-//import java.util.Scanner;
+
 import java.net.URL;                                  //fixa imports, importerar allt for now.
 import java.util.stream.Collectors;
 import java.net.*;
@@ -27,9 +25,9 @@ public class Match {
   private final String        API_KEY   = kr.readKey(); //readKeyReader2();
   private static final String END_POINT = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/";
   private final String        QUERY     = ("34158466?api_key="+ API_KEY); //Insert API Key from KeyReader
+  public String               jsonPath  = "..\\..\\json\\testMatchIdentities.json";
 
-  public String jsonPath = "..\\..\\json\\testMatchIdentities.json";
-  public void JsonParser () /*throws FileNotFoundException*/ {
+  public void JsonParser () {
     try{
         JsonReader reader     = Json.createReader( new FileReader(jsonPath) );
         JsonStructure jsonStruct = reader.read();
@@ -42,14 +40,10 @@ public class Match {
     }catch(JsonException | FileNotFoundException e) {
         System.err.println("File not found: " + e.getMessage());
     }
-  //    JsonArray() jarr = jo.getJsonArray("");
-
   }
 
   public  void  MatchList() {
-
    System.out.println(END_POINT + QUERY);
-
   }
 
 
