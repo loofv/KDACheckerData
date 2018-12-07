359906ae36f3ba12251c73f131019d9a07bc5a03
justicebieber <philipkd@hotmail.com>
2018-12-07 12:53:44 +0100

    Fixed stuff

--- org/utils/Match.java
+++ org/utils/Match.java
@@ -2,6 +2,7 @@ package org.utils;
 
 //import Player;
 //import org.utils.KeyReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -20,7 +21,6 @@ import javax.json.stream.JsonGenerator;
 import javax.json.JsonReader;*/
 import javax.json.*;
 
-
 public class Match {
 
   KeyReader kr = new KeyReader();
@@ -29,26 +29,22 @@ public class Match {
   private final String        QUERY     = ("34158466?api_key="+ API_KEY); //Insert API Key from KeyReader
 
   public String jsonPath = "..\\..\\json\\testMatchIdentities.json";
-  public void JsonParser () /*throws FileNotFoundException*/ { //Denna ska kunna läsa en JSon fil
+  public void JsonParser () /*throws FileNotFoundException*/ {
     try{
-        JsonReader reader     = new Json.createRead( new FileReader(jsonPath));
-        JsonStruct jsonStruct = reader.read();
+        JsonReader reader     = Json.createReader( new FileReader(jsonPath) );
+        JsonStructure jsonStruct = reader.read();
 
-        if (jsonStruct = reader.getValueType().equals(OBJECT)) {
+        if (jsonStruct.getValueType().equals(OBJECT)) {
           System.out.println("Casting to JsonObject...");
-          JsonObject jo = (Json) jsonStruct;
+          JsonObject jo = (JsonObject) jsonStruct;
+          System.out.println("Summonername: " + jo.getString("summonerName"));
         }
-
-        System.out.println("Summonername: " + jo.getString("summonerName"));
-
-    }catch(JsonException e) {
+    }catch(JsonException | FileNotFoundException e) {
         System.err.println("File not found: " + e.getMessage());
     }
   //    JsonArray() jarr = jo.getJsonArray("");
 
   }
-
-
 
   public  void  MatchList() {
 
