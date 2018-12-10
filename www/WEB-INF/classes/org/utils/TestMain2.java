eaef6255073368ec6b1193fa0d3aaf0f10ecc9a5
Love H <love.hulterstrom@gmail.com>
2018-12-10 12:33:08 +0000

    hej

---
+++ www/WEB-INF/classes/org/utils/TestMain2.java
@@ -0,0 +1,15 @@
+package org.utils;
+
+//import org.utils.Match;
+//import org.utils.KeyReader;
+ 
+
+public class TestMain2 {
+
+
+  public static void main (String[] args) {
+   Match m = new Match();
+    m.MatchList();
+    m.JsonParser();
+  }
+}
