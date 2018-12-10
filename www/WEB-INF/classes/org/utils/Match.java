928d4f5d2a3511c2beb5323cc91311cd96bc53a2
U-LAPTOP-R0ISHFM5\loveh <love.hulterstrom@gmail.com>
2018-12-10 16:08:59 +0100

    hej

--- www/WEB-INF/classes/org/utils/Match.java
+++ www/WEB-INF/classes/org/utils/Match.java
@@ -3,8 +3,10 @@ package org.utils;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.net.HttpURLConnection;
 import java.io.FileReader;
+import java.util.List;
 
 import java.net.URL;                                  //fixa imports, importerar allt for now.
 import java.util.stream.Collectors;
@@ -19,15 +21,27 @@ import javax.json.stream.JsonGenerator;
 import javax.json.JsonReader;*/
 import javax.json.*;
 
+
 public class Match {
 
   KeyReader kr = new KeyReader();
   private final String        API_KEY   = kr.readKey();
-  private static final String END_POINT = "https://euw1.api.riotgames.com/lol/match/v3/matchlists/by-account/";
-  private final String        QUERY     = ("34158466?api_key="+ API_KEY); //Insert API Key from KeyReader
+  private static final String END_POINT = "https://euw1.api.riotgames.com/lol/";
+  private String              query;     //= ("34158466?api_key="+ API_KEY); //Insert API Key from KeyReader
   private String              sep       = System.getProperty("file.separator");
   private String              jsonPath  = System.getProperty("user.home")
                                                               + sep + "json" + sep + "testMatchIdentities.json";
+/*  private String urlString;
+
+  public String summonerName;
+  public int accountId;
+  public List <int> MatchList<>;
+  public int kills;
+  public int deaths;
+  public int assists;
+  public double kda; */
+
+
   //public String test1 = "THIS SHOULD BE OVERWRITTEN";
 
   public void JsonParser () {
@@ -38,24 +52,50 @@ public class Match {
         if (jsonStruct.getValueType().equals(OBJECT)) {
           System.out.println("Casting to JsonObject...");
           JsonObject jo = (JsonObject) jsonStruct;
-        //  System.out.println("Summonername: " + jo.getString("summonerName"));
+          //System.out.println("Summonername: " + jo.getString("summonerName"));
 
           final String test1 = jo.toString();
           System.out.println(test1); // För att see att test1 får ett värde från JSON
+
         }
     }catch(JsonException | FileNotFoundException e) {
         System.err.println("File not found: " + e.getMessage() + "\n" + jsonPath );
     }
+  }
+  public void GetStats() {
 
+    String queryAccountId;
+    String queryMatchList;
+    String queryMatchStats;
+    String urlString;
 
+    try {
+        queryAccountId = "summoner/v3/summoners/by-name/loofv?api_key=";
+        urlString = END_POINT + queryAccountId + API_KEY;
+        URL url = new URL (urlString);
+        JsonReader jr = Json.createReader(new InputStreamReader(url.openStream()));
+        System.out.println("junior funkar:" + jr.toString());
+        int accountId;
+        JsonStructure jsonStruct = jr.read();
+        JsonObject jo = (JsonObject) jsonStruct;
+        String name = jo.getString("name");
+        accountId = jo.getInt("accountId");
+        System.out.println("name: " + name + " accountId: " + accountId);
+
+        /*
+         Use accountId to get matchList, put matchIds in an arraylist or other list.
+         iterate over that list of matchIds to get stats for all those matches.
+         Put those stats into a kda-arraylist or other List
+        */
+      } catch (Exception e) {
+            e.printStackTrace();
+    }
   }
 
+
   public  void  MatchList() {
-   System.out.println(END_POINT + QUERY);
+   System.out.println(END_POINT + query);
 //   System.out.println(test1);
   }
 
-
-  //int gameID;
-  //ArrayList <Player> players;
 }
--- www/WEB-INF/classes/org/utils/TestMain2.java
+++ www/WEB-INF/classes/org/utils/TestMain2.java
@@ -2,14 +2,15 @@ package org.utils;
 
 //import org.utils.Match;
 //import org.utils.KeyReader;
- 
+
 
 public class TestMain2 {
 
 
   public static void main (String[] args) {
    Match m = new Match();
-    m.MatchList();
-    m.JsonParser();
+//m.MatchList();
+    //m.JsonParser();
+    m.GetStats();
   }
 }
