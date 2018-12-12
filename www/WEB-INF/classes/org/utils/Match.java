953363a3c892c30768219dc8045359e591be59c8
Love H <love.hulterstrom@gmail.com>
2018-12-12 14:02:07 +0000

    hejhej

--- www/WEB-INF/classes/org/utils/Match.java
+++ www/WEB-INF/classes/org/utils/Match.java
@@ -8,7 +8,6 @@ import java.net.HttpURLConnection;
 import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
-
 import java.net.URL;                                  //fixa imports, importerar allt for now.
 import java.util.stream.Collectors;
 import java.net.*;
@@ -32,18 +31,6 @@ public class Match {
   private String              sep       = System.getProperty("file.separator");
   private String              jsonPath  = System.getProperty("user.home")
                                                               + sep + "json" + sep + "testMatchIdentities.json";
-/*  private String urlString;
-
-  public String summonerName;
-  public int accountId;
-  public List <int> MatchList<>;
-  public int kills;
-  public int deaths;
-  public int assists;
-  public double kda; */
-
-
-  //public String test1 = "THIS SHOULD BE OVERWRITTEN";
 
   public void JsonParser () {
     try{
@@ -71,6 +58,7 @@ public class Match {
     String urlString;
 
     try {
+
         queryAccountId = "summoner/v3/summoners/by-name/loofv?api_key=";
         urlString = END_POINT + queryAccountId + API_KEY;
         URL url = new URL (urlString);
@@ -79,49 +67,43 @@ public class Match {
         int accountId;
         JsonStructure jsonStruct = jr.read();
         JsonObject jo = (JsonObject) jsonStruct;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        SummonerName  = jo.getString("name");
-        accountId     = jo.getInt("accountId");
-        System.out.println("name: "
-                              + SummonerName
-                                + "\n"
-                                  + "accountId: "
-                                    + accountId);
-=======
->>>>>>> fea686a4711318f112750555dbfb15c64ae92dcf
         String name = jo.getString("name");
         accountId = jo.getInt("accountId");
         System.out.println("name: " + name + " accountId: " + accountId);
 
-        queryMatchList = "match/v3/matchlists/by-account/" + accountId + "?api_key=";
 
-        urlString = END_POINT + queryMatchList + API_KEY;
-        /*
-        URL url2 = new URL (urlString);
-        JsonReader jr2 = Json.createReader(new InputStreamReader(url.openStream()));
+        queryMatchList = "match/v3/matchlists/by-account/" + accountId + "?endIndex=10" + "&api_key=";
+        String urlString2 = END_POINT + queryMatchList + API_KEY;
+        System.out.println("why be problem?: " + urlString2);
+        URL url2 = new URL (urlString2);
+        JsonReader jr2 = Json.createReader(new InputStreamReader(url2.openStream()));
         System.out.println("junior2 funkar:" + jr2.toString());
         JsonStructure jsonStruct2 = jr2.read();
         JsonObject jo2 = (JsonObject) jsonStruct2;
-        int matchId = 1337;
-        matchId = jo2.getInt ("matchId");
-        System.out.println(matchId);
-        System.out.println("matchId är: " + matchId);
-        ArrayList <Integer> matchList = new ArrayList<>();
-         */
-         //long gameId i riot api
-       //MatchReferenceDto är json objektet som gameId ligger i
+        System.out.println("\n\n" + jo2.toString());
+
+
         /*
-         Use accountId to get matchList, put matchIds in an arraylist or other list.
-         iterate over that list of matchIds to get stats for all those matches.
-         Put those stats into a kda-arraylist or other List
+        JsonArray ja = new JsonArray (jo2);
+        ArrayList <Integer> matchList = new ArrayList<>();
+        for (int i = 0; i < ja.size(); i++) {
+        }
+        */
+      /*
+        ArrayList <Integer> matchList = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i <jo2.getJsonObject(MatchlistDto).getInt("gameId"); i++) {
+          matchList.add(jo2.getInt("gameId"));
+          j++;
+          System.out.println(matchList.get(i));
+        }
         */
 
-<<<<<<< HEAD
-=======
->>>>>>> 0d3d81b0c31af0eef84414b7c25b814ee2fd40af
->>>>>>> fea686a4711318f112750555dbfb15c64ae92dcf
+
+
+
+
+
       } catch (Exception e) {
             e.printStackTrace();
     }
