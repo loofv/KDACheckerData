0921f6d9e19754079daaa85229a8d19a9182a5e0
loofv <love.hulterstrom@gmail.com>
2018-12-16 11:53:42 +0000

    Skapat ny metod i match, snyggat till calculator

--- www/WEB-INF/classes/org/utils/Match.java
+++ www/WEB-INF/classes/org/utils/Match.java
@@ -1,5 +1,6 @@
 package org.utils;
 
+import org.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -102,6 +103,92 @@ public class Match {
             e.printStackTrace();
     }
   }
+//------------------------testar ett sätt att fixa matchlista----------------------
+//skickar en arraylist med kda för 10 matcher i stringform. körs i main.
+// till stor del samma kod som getstats() metoden ovan 
+
+
+public ArrayList<String> GetStatsList() {
+  ArrayList<String> statsList = new ArrayList<>();
+  Calculator c = new Calculator();
+  String queryAccountId;
+  String queryMatchList;
+  String queryMatchStats;
+  String urlString;
+  String summonerName;
+  try {
+      summonerName = "lOOFv";
+      queryAccountId = "summoner/v3/summoners/by-name/"+ summonerName +"?api_key=";
+      urlString = END_POINT + queryAccountId + API_KEY;
+      URL url = new URL (urlString);
+      StringBuilder  sb = new StringBuilder();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      for (String line : reader.lines().collect(Collectors.toList())) {
+        sb.append(line);
+      }
+        System.out.println("\n" + sb + "\n");
+/* ====================================================================================================== */
+      JSONObject jo = new JSONObject(sb.toString());
+      long accountId = jo.getLong("accountId");
+      queryMatchList = "match/v3/matchlists/by-account/" + accountId + "?endIndex=10" + "&api_key=";
+      urlString = END_POINT + queryMatchList + API_KEY;
+      url = new URL (urlString);
+      reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      sb = new StringBuilder();
+      for (String line : reader.lines().collect(Collectors.toList())) {
+        sb.append(line);
+      }
+      jo = new JSONObject(sb.toString());
+      JSONArray ja = jo.getJSONArray("matches");
+      for (int i = 0; i < ja.length(); i++) {
+          JSONObject match = ja.getJSONObject(i);
+          System.out.println("GameId: "+ match.getLong("gameId") + "\n");
+
+      }
+/* ====================================================================================================== */
+
+      long matchId;
+      int pID;
+      reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      for (int i = 0; i < ja.length(); i++){
+          JSONObject matchStats = ja.getJSONObject(i);
+          queryMatchStats = "match/v3/matches/" + matchStats.getLong("gameId") + "?api_key=";
+          urlString = END_POINT + queryMatchStats + API_KEY;
+          url = new URL (urlString);
+          reader = new BufferedReader(new InputStreamReader(url.openStream()));
+          sb = new StringBuilder();
+          int u = 0;
+
+          for (String line : reader.lines().collect(Collectors.toList())) {
+
+              sb.append(line);
+              jo = new JSONObject(sb.toString());
+              JSONArray participants = (jo.getJSONArray("participants"));
+              JSONArray participantIdentities = (jo.getJSONArray("participantIdentities"));
+
+              for (int x = 0; x < participantIdentities.length(); x++) {
+                if (participantIdentities.getJSONObject(x).getJSONObject("player").getString("summonerName").equals(summonerName)) {
+                    pID = participantIdentities.getJSONObject(x).getInt("participantId");
+                    for (int y = 0; y < participants.length(); y++) {
+                      if (participants.getJSONObject(y).getInt("participantId") == (pID)) {
+                        int kills = participants.getJSONObject(y).getJSONObject("stats").getInt("kills");
+                        int assists = participants.getJSONObject(y).getJSONObject("stats").getInt("assists");
+                        int deaths =  participants.getJSONObject(y).getJSONObject("stats").getInt("deaths");
+
+                        double kda = c.calculateKda(kills, assists, deaths);
+                        statsList.add(String.valueOf(kda)); //ändrar doublen till string, annars får vi ej ha arraylist.
+                      }
+                    }
+                  }
+            }
+          }
+        }
+      } catch (Exception e) {
+          e.printStackTrace();
+  }
+  return statsList;
+}
+
 
   public  void  MatchList() {
    System.out.println(END_POINT + query);
