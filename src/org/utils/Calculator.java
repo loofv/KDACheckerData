ffd86eda003dc58f778e9ca1b273b01872001a22
loofv <love.hulterstrom@gmail.com>
2018-12-30 19:55:37 +0000

    lagt till kommentarer, rensat imports

--- src/org/utils/Calculator.java
+++ src/org/utils/Calculator.java
@@ -1,7 +1,5 @@
 package org.utils;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.lang.Math;
 
 public class Calculator {
@@ -10,17 +8,16 @@ public class Calculator {
   public int deaths;
   public int assists;
   public double kda;
-  ArrayList<String> listKda;
 
   public double calculateKda (int kills, int deaths, int assists) {
     double k = kills;
     double d = deaths;
     double a = assists;
 
-// smått grisig if-sats, finns troligen bättre lösning.
+
     if (d == 0.0) {
       System.out.println("Your kda is perfect! (100)");
-      kda = 100.0; //sätter kda 100 om man inte dör. annan lösning?
+      kda = 100.0; //sätter kda 100 om man inte dör.
       return kda;
     } else {
     kda = ((k + a) / d);
